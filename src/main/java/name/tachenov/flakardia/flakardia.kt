9d2e94b92fc2f7efd57e61effa6a895a61f4f8fe
Sergei Tachenov <sergei@tachenov.name>
2024-01-28 14:59:08 +0200

    Configure font

--- src/main/java/name/tachenov/flakardia/flakardia.kt
+++ src/main/java/name/tachenov/flakardia/flakardia.kt
@@ -6,6 +6,9 @@ import com.github.weisj.darklaf.theme.OneDarkTheme
 import com.github.weisj.darklaf.theme.info.DefaultThemeProvider
 import name.tachenov.flakardia.ui.CardSetManagerFrame
 import java.awt.Font
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.*
 import javax.swing.SwingUtilities
 import javax.swing.UIManager
 import javax.swing.WindowConstants
@@ -25,11 +28,40 @@ fun main() {
 }
 
 private fun configureUiDefaults() {
+    val configuration = Properties()
+    val configPath = Path.of("flakardia.properties")
+    if (Files.isReadable(configPath)) {
+        Files.newBufferedReader(configPath).use {
+            configuration.load(it)
+        }
+    }
     val uiDefaults = UIManager.getDefaults()
-    uiDefaults["Button.font"] = Font("Verdana", Font.PLAIN, 16)
-    uiDefaults["List.font"] = Font("Verdana", Font.PLAIN, 16)
-    uiDefaults["TableHeader.font"] = Font("Verdana", Font.PLAIN, 16)
-    uiDefaults["Table.font"] = Font("Verdana", Font.PLAIN, 16)
-    uiDefaults["Label.font"] = Font("Verdana", Font.PLAIN, 16)
-    uiDefaults["TextField.font"] = Font("Verdana", Font.PLAIN, 16)
+    val font = parseFont(configuration.getProperty("font", "Verdana 16"))
+    uiDefaults["Button.font"] = font
+    uiDefaults["List.font"] = font
+    uiDefaults["TableHeader.font"] = font
+    uiDefaults["Table.font"] = font
+    uiDefaults["Label.font"] = font
+    uiDefaults["TextField.font"] = font
+}
+
+fun parseFont(s: String): Font {
+    var trimmed = s.trim()
+    var styleValue = 0
+    while (true) {
+        val style = FontStyle.entries.firstOrNull { style -> trimmed.uppercase().endsWith(" $style")}
+        if (style == null) {
+            break
+        }
+        styleValue = styleValue or style.value
+        trimmed = trimmed.removeRange(trimmed.length - style.name.length, trimmed.length).trim()
+    }
+    val lastSpace = trimmed.lastIndexOf(' ')
+    val size = trimmed.substring(lastSpace + 1).toInt()
+    return Font(trimmed.substring(0, lastSpace).trim(), styleValue, size)
+}
+
+private enum class FontStyle(val value: Int) {
+    BOLD(Font.BOLD),
+    ITALIC(Font.ITALIC);
 }
