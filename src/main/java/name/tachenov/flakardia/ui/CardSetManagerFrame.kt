61ddd1676a0eb3a1a1147eb0a7c8457ae4873b3b
Sergei Tachenov <sergei@tachenov.name>
2024-05-04 13:54:12 +0300

    0.5.1-SNAPSHOT

--- src/main/java/name/tachenov/flakardia/ui/CardSetManagerFrame.kt
+++ src/main/java/name/tachenov/flakardia/ui/CardSetManagerFrame.kt
@@ -23,7 +23,7 @@ import kotlin.math.max
 class CardSetManagerFrame(
     private val manager: CardManager,
     private val service: FlashcardService,
-) : JFrame("Flakardia 0.5") {
+) : JFrame("Flakardia 0.5.1") {
 
     private val dir = JLabel()
     private val list = JList<CardListEntryView>()
