5662453699d93694241e5ac7926e1ea92eb830ca
Sergei Tachenov <sergei@tachenov.name>
2025-03-23 08:19:45 +0200

    Increase frame extra width
    Sometimes it still wasn't enough, causing the horizontal scroll bar
    to appear. We could fix the calculation, of course,
    but given the Swing nature it's not an easy thing to do
    and it would be a fragile fix working only until the next redesign.
    
    Having an extra 50 pixels, on the other hand, looks nice and works reliably.

--- src/main/java/name/tachenov/flakardia/ui/uiConstants.kt
+++ src/main/java/name/tachenov/flakardia/ui/uiConstants.kt
@@ -8,7 +8,7 @@ const val INFINITY = 100_000
 /**
  * Extra width to add to a packed frame to avoid relying on tight calculations.
  */
-const val FRAME_EXTRA_WIDTH = 10
+const val FRAME_EXTRA_WIDTH = 50
 
 val CORRECT_COLOR = Color(33, 169, 10)
 val INCORRECT_COLOR = Color(222, 15, 15)
