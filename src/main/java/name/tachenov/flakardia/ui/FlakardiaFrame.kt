8f6bd8cb66c911ef60de1d37e08023ce49a2cded
Sergei Tachenov <sergei.tachenov@jetbrains.com>
2025-03-24 10:37:55 +0200

    Refactoring: move setupShortcuts
    Now that we have uiUtil.kt, it seems the best place.

--- src/main/java/name/tachenov/flakardia/ui/FlakardiaFrame.kt
+++ src/main/java/name/tachenov/flakardia/ui/FlakardiaFrame.kt
@@ -1,30 +1,11 @@
 package name.tachenov.flakardia.ui
 
-import java.awt.Window
-import java.awt.event.ActionEvent
-import java.awt.event.KeyEvent
-import javax.swing.AbstractAction
-import javax.swing.JComponent
 import javax.swing.JFrame
-import javax.swing.JRootPane
-import javax.swing.KeyStroke
 
 open class FlakardiaFrame : JFrame() {
 
     init {
         defaultCloseOperation = DISPOSE_ON_CLOSE
         rootPane.setupShortcuts(this)
-    }
-}
-
-fun JRootPane.setupShortcuts(window: Window) {
-    if (System.getProperty("os.name").startsWith("mac", ignoreCase = true)) {
-        rootPane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW)
-            .put(KeyStroke.getKeyStroke(KeyEvent.VK_W, KeyEvent.META_DOWN_MASK), "close")
-        rootPane.actionMap.put("close", object : AbstractAction() {
-            override fun actionPerformed(e: ActionEvent?) {
-                window.dispose()
-            }
-        })
     }
 }
--- src/main/java/name/tachenov/flakardia/ui/uiUtil.kt
+++ src/main/java/name/tachenov/flakardia/ui/uiUtil.kt
@@ -3,8 +3,25 @@ package name.tachenov.flakardia.ui
 import java.awt.Point
 import java.awt.Rectangle
 import java.awt.Window
+import java.awt.event.ActionEvent
+import java.awt.event.KeyEvent
+import javax.swing.AbstractAction
+import javax.swing.JComponent
+import javax.swing.JRootPane
+import javax.swing.KeyStroke
 import kotlin.math.abs
 
+fun JRootPane.setupShortcuts(window: Window) {
+    if (System.getProperty("os.name").startsWith("mac", ignoreCase = true)) {
+        rootPane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW)
+            .put(KeyStroke.getKeyStroke(KeyEvent.VK_W, KeyEvent.META_DOWN_MASK), "close")
+        rootPane.actionMap.put("close", object : AbstractAction() {
+            override fun actionPerformed(e: ActionEvent?) {
+                window.dispose()
+            }
+        })
+    }
+}
 
 fun Window.setLocationAboveOrBelowOf(owner: Window?) {
     val location = owner?.findLocationAboveOrBelow(width, height)
